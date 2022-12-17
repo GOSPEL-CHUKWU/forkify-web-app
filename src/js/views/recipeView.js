44e728f90a1fc9b8ece908fabe596e2083bb8319
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2022-12-18 00:07:58 +0100

    Lot of change

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; //Parcel 1
 import icons from 'url:../../img/icons.svg'; //Parcel 2
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 // console.log(Fraction);
 
 class RecipeView extends View {
@@ -127,13 +127,14 @@ class RecipeView extends View {
   }
 
   _generateMarkupIngredient(ing) {
+    console.log(fracty(0.4));
     return `
           <li class="recipe__ingredient">
           <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            !ing.quantity ? '' : new Fraction(ing.quantity).toString()
+            !ing.quantity ? '' : fracty(ing.quantity).toString()
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
